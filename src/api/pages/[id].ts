3cd0bc98438680237545b9ba8c15cb1026425d99
berdibek-2002 <uralov0212@gmail.com>
2024-12-21 09:36:18 +0500

    file add function v1.0.4

--- src/api/pages/[id].ts
+++ src/api/pages/[id].ts
@@ -8,29 +8,27 @@ interface UpdatePageData {
   path?: string;
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { id } = req.query;
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   const { id } = req.query;
 
-  const parsedId = typeof id === 'string' ? parseInt(id, 10) : id;
+//   if (req.method === 'PUT') {
+//     const { name, path }: UpdatePageData = req.body;
 
-  if (req.method === 'PUT') {
-    const { name, path }: UpdatePageData = req.body;
+//     try {
+//       const updatedPage = await prisma.page.update({
+//         where: { id: String(id) },
+//         data: {
+//           ...(name && { name }),
+//           ...(path && { path }),
+//           updateDate: new Date(),
+//         },
+//       });
 
-    try {
-      const updatedPage = await prisma.page.update({
-        where: { id: String(id) },
-        data: {
-          ...(name && { name }),
-          ...(path && { path }),
-          updateDate: new Date(),
-        },
-      });
-
-      return res.status(200).json(updatedPage);
-    } catch (error) {
-      return res.status(500).json({ error: 'Yangilashda xatolik yuz berdi' });
-    }
-  } else {
-    return res.status(405).json({ error: 'Metod qo‘llab-quvvatlanmaydi' });
-  }
-}
+//       return res.status(200).json(updatedPage);
+//     } catch (error) {
+//       return res.status(500).json({ error: 'Yangilashda xatolik yuz berdi' });
+//     }
+//   } else {
+//     return res.status(405).json({ error: 'Metod qo‘llab-quvvatlanmaydi' });
+//   }
+// }
